6b07727e573292d270f9b4bec27ec171a8e9f43d
Emre Süslü <emresuslu1480@gmail.com>
2023-04-16 14:14:02 +0300

    styling location changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bulma/css/bulma.min.css';
 import './css/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
